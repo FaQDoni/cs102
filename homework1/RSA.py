ff66aa650a93af1e638d5b272cd47a6dbc00b9af
IkonenkoDanil <dan-ikonenko@mail.ru>
2019-10-12 19:38:10 +0300

    Update RSA.py

--- homework1/RSA.py
+++ homework1/RSA.py
@@ -1,83 +1,116 @@
 import random
 
 
-def is_prime(n: int) -> bool:
+def is_prime(n):
     """
-      >>> is_prime(2)
-      True
-      >>> is_prime(11)
-      True
-      >>> is_prime(8)
-      False
-      """
+    Tests to see if a number is prime.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    
+    pr = True
+    if n > 3:
+        for i in range(2, n // 2 + 1):
+            if n % i == 0:
+                pr = False
+                break
+    elif n <= 1:
+        pr = False
+    return pr
 
 
-    z=bool
-    Check=0
-    for i in range(1,n+1):
-        if (n % i)==0:
-            Check=Check+1
-    if Check==2:
-        z=True
-    else:
-        print("Your number is wrong!!!",n)
-    pass
+def gcd(a, b):
+    """
+    Euclid's algorithm for determining the greatest common divisor.
 
-def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    
+    while a != 0 and b != 0:
+        if a > b:
+            a %= b
+        else:
+            b %= a  
+    return (max(a, b))
+
+def multiplicative_inverse(e, phi):
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
+
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    
+    a = phi
+    b = e
+    arr = [] 
+    while a % b != 0:
+        arr.append(a // b)
+        tmp = a
+        a = b
+        b = tmp % b
+    x = 0
+    y = 1
+    for i in range(len(arr)):
+        tmp = x
+        x = y
+        y = tmp - y * arr[len(arr) - i - 1]
+    return y % phi
+
+
+def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
         raise ValueError('Both numbers must be prime.')
     elif p == q:
         raise ValueError('p and q cannot be equal')
-    n = p*q
-    phi = (p-1)(q-1)
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+
+    
     e = random.randrange(1, phi)
 
-
-    def gcd(a: int, b: int) -> int:
-
-        """
-          >>> gcd(12, 15)
-          3
-          >>> gcd(3, 7)
-          1
-          """
-
-
-        x=a
-        y=b
-        while y>0:
-            z=x%y
-            x,y=y,z
-        a=x                        
-        return a
+    
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-
-    def multiplicative_inverse(e: int, phi: int) -> int:
-
-        """
-           >>> multiplicative_inverse(7, 40)
-           23
-           """
-
-
-
-        x=e
-        y=phi
-        z=2 
-        while z*x%y!=1:
-             z+=1
-        d=z         
-    pass
     d = multiplicative_inverse(e, phi)
     return ((e, n), (d, n))
 
-is_prime(2)
-is_prime(11)
-is_prime(8)
-gcd(12, 15)
-gcd(3, 7)
-multiplicative_inverse(7, 40)
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
