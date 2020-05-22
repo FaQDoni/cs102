3e2b77a51a07d61349ceca16e059d5bd7a534df3
IkonenkoDanil <dan-ikonenko@mail.ru>
2020-05-22 14:00:48 +0300

    Add files via upload

---
+++ homework7/manage.py
@@ -0,0 +1,20 @@
+import falcon
+import json
+
+
+class QuoteResource:
+
+    @staticmethod
+    def on_get(resp):
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+        resp.body = json.dumps(quote)
+
+    def on_post(self, req, resp):
+        pass
+
+
+api = falcon.API()
+api.add_route('/quote', QuoteResource())
